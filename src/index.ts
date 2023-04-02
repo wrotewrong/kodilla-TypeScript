07ed41f4c4bcbfb07284cd90666013fc18fa99f5
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-04-02 20:21:19 +0200

    Add Message class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const consola = require('consola');
 
 enum Action {
   List = 'list',
@@ -28,3 +29,47 @@ const startApp = () => {
 };
 
 startApp();
+
+enum MessageVariant {
+  Success = 'success',
+  Error = 'error',
+  Info = 'info',
+}
+
+class Message {
+  private content: string;
+
+  constructor(content: string) {
+    this.content = content;
+  }
+
+  public show(): void {
+    console.log(this.content);
+  }
+
+  public capitalize(): void {
+    const splitContent: string[] = this.content.split('');
+    const first = splitContent[0].toUpperCase();
+    splitContent.shift();
+    const rest = splitContent.join('').toLowerCase();
+    this.content = first + rest;
+  }
+
+  public toUpperCase(): void {
+    this.content = this.content.toUpperCase();
+  }
+
+  public toLowerCase(): void {
+    this.content = this.content.toLowerCase();
+  }
+
+  static showColorized(variant: MessageVariant, text: string): void {
+    if (variant === 'success') {
+      consola.success(text);
+    } else if (variant === 'error') {
+      consola.error(text);
+    } else {
+      consola.info(text);
+    }
+  }
+}
